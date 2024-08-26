1bb6d64cf3bfbc2567d111feda653872380710cc
swrrt <swrrt.11@gmail.com>
2024-08-26 13:10:21 +0800

    system sensitivity local

---
+++ streamsluice_scripts/scripts/sensitivity-local.sh
@@ -0,0 +1,208 @@
+#!/bin/bash
+
+source config-systemsensitivity-local.sh
+
+# dump data
+function analyze() {
+    mkdir -p ${EXP_DIR}/raw/
+    mkdir -p ${EXP_DIR}/results/
+
+    echo "INFO: dump to ${EXP_DIR}/raw/${EXP_NAME}"
+    if [[ -d ${EXP_DIR}/raw/${EXP_NAME} ]]; then
+        rm -rf ${EXP_DIR}/raw/${EXP_NAME}
+    fi
+    mv ${FLINK_DIR}/log/* ${EXP_DIR}/streamsluice/
+    mv ${EXP_DIR}/streamsluice/ ${EXP_DIR}/raw/${EXP_NAME}
+    mkdir ${EXP_DIR}/streamsluice/
+
+#    for host in "dragon" "eagle"; do
+#      scp ${host}:${FLINK_DIR}/log/* ${EXP_DIR}/raw/${EXP_NAME}/
+#      ssh ${host} "rm ${FLINK_DIR}/log/*"
+#    done
+}
+
+run_one_exp() {
+  EXP_NAME=system_local-${whether_type}-${how_type}-${SOURCE_TYPE}-${GRAPH}-${runtime}-${RATE1}-${RATE2}-${RATE_I}-${P1}-${ZIPF_SKEW}-${P2}-${DELAY2}-${IO2}-${STATE_SIZE2}-${P3}-${DELAY3}-${IO3}-${STATE_SIZE3}-${P4}-${DELAY4}-${IO4}-${STATE_SIZE4}-${P5}-${DELAY5}-${STATE_SIZE5}-${L}-${migration_interval}-${epoch}-${decision_interval}-${is_treat}-${repeat}
+
+  echo "INFO: run exp ${EXP_NAME}"
+  configFlink
+  runFlink
+
+  python -c 'import time; time.sleep(5)'
+
+  runApp
+
+  SCRIPTS_RUNTIME=`expr ${runtime} + 10`
+  python -c 'import time; time.sleep('"${SCRIPTS_RUNTIME}"')'
+
+  analyze
+  stopFlink
+
+  python -c 'import time; time.sleep(5)'
+}
+
+# initialization of the parameters
+init() {
+  # exp scenario
+  controller_type=StreamSluice
+  whether_type="streamsluice"
+  how_type="streamsluice"
+  vertex_id="a84740bacf923e828852cc4966f2247c,eabd4c11f6c6fbdf011f0f1fc42097b1,d01047f852abd5702a0dabeedac99ff5"
+  L=1000
+  migration_interval=500
+  epoch=100
+  # app level
+  JAR="${FLINK_APP_DIR}/target/testbed-1.0-SNAPSHOT.jar"
+  job="flinkapp.StreamSluiceTestSet.MicroBench"
+  # only used in script
+  runtime=300
+  # set in Flink app
+  GRAPH=3op
+  ZIPF_SKEW=0
+  NKEYS=1000
+  P1=1
+  MP1=1
+
+  P2=2
+  MP2=128
+  DELAY2=300
+  IO2=1
+  STATE_SIZE2=1000
+
+  P3=2
+  MP3=128
+  DELAY3=300
+  IO3=1
+  STATE_SIZE3=1000
+
+  P4=2
+  MP4=128
+  DELAY4=300
+  IO4=1
+  STATE_SIZE4=1000
+
+  P5=6
+  MP5=128
+  DELAY5=510
+  STATE_SIZE5=1000
+
+  spike_estimation="linear_regression"
+  spike_slope=0.65
+  spike_intercept=250
+  is_treat=true
+  repeat=1
+  warmup=10000
+}
+
+# run applications
+function runApp() {
+    echo "INFO: ${FLINK_DIR}/bin/flink run -c ${job} ${JAR} \
+    -graph ${GRAPH} \
+    -p1 ${P1} -mp1 ${MP1} -p2 ${P2} -mp2 ${MP2} -op2Delay ${DELAY2} -op2IoRate ${IO2} -op2KeyStateSize ${STATE_SIZE2} \
+    -p3 ${P3} -mp3 ${MP3} -op3Delay ${DELAY3} -op3IoRate ${IO3} -op3KeyStateSize ${STATE_SIZE3} \
+    -p4 ${P4} -mp4 ${MP4} -op4Delay ${DELAY4} -op4IoRate ${IO4} -op4KeyStateSize ${STATE_SIZE4} \
+    -nkeys ${NKEYS} -phase1Time ${TIME1} -phase1Rate ${RATE1} -phase2Time ${TIME2} \
+    -phase2Rate ${RATE2} -interTime ${TIME_I} -interRate ${RATE_I} \
+    -source ${SOURCE_TYPE} \
+    -zipf_skew ${ZIPF_SKEW} &"
+    ${FLINK_DIR}/bin/flink run -c ${job} ${JAR} \
+    -graph ${GRAPH} \
+    -p1 ${P1} -mp1 ${MP1} -p2 ${P2} -mp2 ${MP2} -op2Delay ${DELAY2} -op2IoRate ${IO2} -op2KeyStateSize ${STATE_SIZE2} \
+    -p3 ${P3} -mp3 ${MP3} -op3Delay ${DELAY3} -op3IoRate ${IO3} -op3KeyStateSize ${STATE_SIZE3} \
+    -p4 ${P4} -mp4 ${MP4} -op4Delay ${DELAY4} -op4IoRate ${IO4} -op4KeyStateSize ${STATE_SIZE4} \
+    -p5 ${P5} -mp5 ${MP5} -op5Delay ${DELAY5} -op5KeyStateSize ${STATE_SIZE5} \
+    -nkeys ${NKEYS} -phase1Time ${TIME1} -phase1Rate ${RATE1} -phase2Time ${TIME2} \
+    -phase2Rate ${RATE2} -interTime ${TIME_I} -interRate ${RATE_I}\
+    -source ${SOURCE_TYPE} \
+    -zipf_skew ${ZIPF_SKEW} &
+}
+
+run_scale_test(){
+    echo "Run micro bench system sensitivity..."
+    init
+    #L=1000
+    #is_treat=false
+    #repeat=1
+    #run_one_exp
+
+
+    # Different cases
+    GRAPH="1split2join1"
+    SOURCE_TYPE="when"
+    CURVE_TYPE="a"
+    vertex_id="a84740bacf923e828852cc4966f2247c,eabd4c11f6c6fbdf011f0f1fc42097b1,d01047f852abd5702a0dabeedac99ff5,d2336f79a0d60b5a4b16c8769ec82e47"
+    autotune=false
+    epoch=100
+    decision_interval=10
+    snapshot_size=3
+
+    L=2000 #2500
+    migration_interval=3000
+    spike_slope=0.7
+    #spike_intercept=200
+    #STATE_SIZE2=1000
+    #STATE_SIZE3=1000
+    #STATE_SIZE4=1000
+    #spike_intercept=1500 #1200 #750
+
+#    STATE_SIZE2=10000
+#    STATE_SIZE3=10000
+#    STATE_SIZE4=10000
+#    STATE_SIZE5=10000
+    STATE_SIZE2=5000
+    STATE_SIZE3=5000
+    STATE_SIZE4=5000
+    STATE_SIZE5=5000
+    spike_intercept=1000
+    runtime=400
+    DELTA_I=270
+    LP2=2
+    LP3=2
+    LP4=2
+    LP5=13
+
+    RATE1=6000
+    TIME1=30
+    RATE2=3000
+    TIME2=30
+    RATE_I=4000
+    TIME_I=30
+    printf "" > whetherhow_result.txt
+
+    # Curve 1
+    SOURCE_TYPE="when"
+    is_treat=false
+    run_one_exp
+    printf "${EXP_NAME}\n" >> whetherhow_result.txt
+#    is_treat=false
+#    run_one_exp
+#    printf "${EXP_NAME}\n" >> whetherhow_result.txt
+#    is_treat=true
+#    whether_early="streamsluice_earlier"
+#    whether_late="streamsluice_later"
+#    for whether_type in ${whether_early} ${whether_late}; do
+#      #how_type="streamsluice"
+#      run_one_exp
+#      printf "${EXP_NAME}\n" >> whetherhow_result.txt
+#    done
+#    whether_type="streamsluice"
+
+    RATE1=6000
+    RATE2=3000
+    RATE_I=4000
+    SOURCE_TYPE="how"
+    run_one_exp
+    printf "${EXP_NAME}\n" >> whetherhow_result.txt
+#    is_treat=false
+#    run_one_exp
+#    printf "${EXP_NAME}\n" >> whetherhow_result.txt
+#    is_treat=true
+#    for how_type in "streamsluice_not_bottleneck" "streamsluice_less" "streamsluice_no_balance"  "streamsluice_more"; do #"streamsluice_minus_one" ; do #  "streamsluice_not_bottleneck"; do
+#      run_one_exp
+#      printf "${EXP_NAME}\n" >> whetherhow_result.txt
+#    done
+#    how_type="streamsluice"
+}
+
+run_scale_test
+
