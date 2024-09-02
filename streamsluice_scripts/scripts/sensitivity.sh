607152f7e56461a8b4c722521d923e4c44e804f9
swrrt <swrrt.11@gmail.com>
2024-09-02 13:46:18 +0800

    Debugging

--- streamsluice_scripts/scripts/sensitivity.sh
+++ streamsluice_scripts/scripts/sensitivity.sh
@@ -22,7 +22,7 @@ function analyze() {
 }
 
 run_one_exp() {
-  EXP_NAME=system-${whether_type}-${how_type}-${SOURCE_TYPE}-${GRAPH}-${runtime}-${RATE1}-${RATE2}-${RATE_I}-${P1}-${ZIPF_SKEW}-${P2}-${DELAY2}-${IO2}-${STATE_SIZE2}-${P3}-${DELAY3}-${IO3}-${STATE_SIZE3}-${P4}-${DELAY4}-${IO4}-${STATE_SIZE4}-${P5}-${DELAY5}-${STATE_SIZE5}-${L}-${migration_interval}-${epoch}-${decision_interval}-${is_treat}-${repeat}
+  EXP_NAME=system-${whether_type}-${how_type}-${how_conservative_flag}-${how_optimization_flag}-${SOURCE_TYPE}-${GRAPH}-${runtime}-${RATE1}-${RATE2}-${RATE_I}-${P1}-${ZIPF_SKEW}-${P2}-${DELAY2}-${IO2}-${STATE_SIZE2}-${P3}-${DELAY3}-${IO3}-${STATE_SIZE3}-${P4}-${DELAY4}-${IO4}-${STATE_SIZE4}-${P5}-${DELAY5}-${STATE_SIZE5}-${L}-${migration_interval}-${epoch}-${decision_interval}-${is_treat}-${repeat}
 
   echo "INFO: run exp ${EXP_NAME}"
   configFlink
@@ -173,7 +173,7 @@ run_scale_test(){
     how_conservative_flag=true
     # Curve 1
     SOURCE_TYPE="when"
-    is_treat=false
+    is_treat=true
     run_one_exp
     printf "${EXP_NAME}\n" >> whetherhow_result.txt
 #    is_treat=false
