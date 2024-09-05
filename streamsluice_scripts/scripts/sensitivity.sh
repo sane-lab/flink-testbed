e8538746d9197269488ef9819fe4db5cc17b5fee
swrrt <swrrt.11@gmail.com>
2024-09-05 21:56:22 +0800

    Debugging

--- streamsluice_scripts/scripts/sensitivity.sh
+++ streamsluice_scripts/scripts/sensitivity.sh
@@ -22,7 +22,7 @@ function analyze() {
 }
 
 run_one_exp() {
-  EXP_NAME=system-${whether_type}-${how_type}-${how_conservative_flag}-${how_steady_limit_flag}-${how_optimization_flag}-${SOURCE_TYPE}-${CURVE_TYPE}-${GRAPH}-${runtime}-${RATE1}-${RATE2}-${RATE_I}-${P1}-${ZIPF_SKEW}-${P2}-${DELAY2}-${IO2}-${STATE_SIZE2}-${P3}-${DELAY3}-${IO3}-${STATE_SIZE3}-${P4}-${DELAY4}-${IO4}-${STATE_SIZE4}-${P5}-${DELAY5}-${STATE_SIZE5}-${L}-${migration_interval}-${epoch}-${decision_interval}-${is_treat}-${repeat}
+  EXP_NAME=system-${coordination_latency_flag}-${whether_type}-${how_type}-${how_conservative_flag}-${how_steady_limit_flag}-${how_optimization_flag}-${SOURCE_TYPE}-${CURVE_TYPE}-${GRAPH}-${runtime}-${RATE1}-${RATE2}-${RATE_I}-${P1}-${ZIPF_SKEW}-${P2}-${DELAY2}-${IO2}-${STATE_SIZE2}-${P3}-${DELAY3}-${IO3}-${STATE_SIZE3}-${P4}-${DELAY4}-${IO4}-${STATE_SIZE4}-${P5}-${DELAY5}-${STATE_SIZE5}-${L}-${migration_interval}-${epoch}-${decision_interval}-${is_treat}-${repeat}
 
   echo "INFO: run exp ${EXP_NAME}"
   configFlink
@@ -162,11 +162,11 @@ run_scale_test(){
     LP5=13
 
     RATE1=6000
-    TIME1=45 #30
+    TIME1=30
     RATE2=3000
-    TIME2=45 #30
+    TIME2=30
     RATE_I=4000
-    TIME_I=45 #30
+    TIME_I=30
     printf "" > whetherhow_result.txt
     how_more_optimization_flag=false
     how_optimization_flag=false
