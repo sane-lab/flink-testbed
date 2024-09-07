10ecb8e53181b3b150e89883eb7a26928783bf19
swrrt <swrrt.11@gmail.com>
2024-09-07 14:18:32 +0800

    Debugging

--- streamsluice_scripts/scripts/sensitivity.sh
+++ streamsluice_scripts/scripts/sensitivity.sh
@@ -22,7 +22,7 @@ function analyze() {
 }
 
 run_one_exp() {
-  EXP_NAME=system-${coordination_latency_flag}-${whether_type}-${how_type}-${how_conservative_flag}-${how_steady_limit_flag}-${how_optimization_flag}-${SOURCE_TYPE}-${CURVE_TYPE}-${GRAPH}-${runtime}-${RATE1}-${RATE2}-${RATE_I}-${P1}-${ZIPF_SKEW}-${P2}-${DELAY2}-${IO2}-${STATE_SIZE2}-${P3}-${DELAY3}-${IO3}-${STATE_SIZE3}-${P4}-${DELAY4}-${IO4}-${STATE_SIZE4}-${P5}-${DELAY5}-${STATE_SIZE5}-${L}-${migration_interval}-${epoch}-${decision_interval}-${is_treat}-${repeat}
+  EXP_NAME=system-${coordination_latency_flag}-${whether_type}-${how_type}-${how_conservative_flag}-${how_steady_limit_flag}-${conservative_service_rate_flag}-${SOURCE_TYPE}-${CURVE_TYPE}-${GRAPH}-${runtime}-${RATE1}-${RATE2}-${RATE_I}-${P1}-${ZIPF_SKEW}-${P2}-${DELAY2}-${IO2}-${STATE_SIZE2}-${P3}-${DELAY3}-${IO3}-${STATE_SIZE3}-${P4}-${DELAY4}-${IO4}-${STATE_SIZE4}-${P5}-${DELAY5}-${STATE_SIZE5}-${L}-${migration_interval}-${epoch}-${decision_interval}-${is_treat}-${repeat}
 
   echo "INFO: run exp ${EXP_NAME}"
   configFlink
@@ -154,7 +154,7 @@ run_scale_test(){
     STATE_SIZE4=5000
     STATE_SIZE5=5000
     spike_intercept=1000
-    runtime=760 #520 #400
+    runtime=520 #520 #400
     DELTA_I=270
     LP2=2
     LP3=2
@@ -178,8 +178,8 @@ run_scale_test(){
     SOURCE_TYPE="when"
     is_treat=false
     how_type="ds2"
-    run_one_exp
-    printf "${EXP_NAME}\n" >> whetherhow_result.txt
+#    run_one_exp
+#    printf "${EXP_NAME}\n" >> whetherhow_result.txt
     is_treat=true
     how_type="streamsluice"
     for L in 2000 1000 750 500; do
