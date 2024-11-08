44692758a1285cab6f9c47888fe592c51207896a
swrrt <swrrt.11@gmail.com>
2024-11-08 23:59:38 +0800

    Debugging

--- streamsluice_scripts/scripts/twitter-alert.sh
+++ streamsluice_scripts/scripts/twitter-alert.sh
@@ -49,7 +49,7 @@ init() {
   how_type="streamsluice"
   scalein_type="streamsluice"
   L=2000 #4000
-  runtime=750 #1950
+  runtime=1950 #750
   skip_interval=1 # skip seconds
   warmup=10000
   warmup_time=90
@@ -159,7 +159,7 @@ run_stock_test(){
     autotune=true
     repeat=2
     for scaling_decision_option in 1; do # 2 0
-      for autotuner_increase_bar_alpha in 0.1; do # 0.1 0.2 0.4
+      for autotuner_increase_bar_alpha in 0.1 0.2 0.4; do # 0.1 0.2 0.4
         for L in 750 1000 1500 2000 2500; do #500 750 1000 1500 2000 2500; do
             whether_type="streamsluice"
             how_type="streamsluice"
