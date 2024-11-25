f8282f0cbe5a0111039c86a23db9c492edbd65ce
swrrt <swrrt.11@gmail.com>
2024-11-25 19:25:35 +0800

    Debugging

--- streamsluice_scripts/scripts/twitter-alert.sh
+++ streamsluice_scripts/scripts/twitter-alert.sh
@@ -160,7 +160,7 @@ run_stock_test(){
     repeat=1
     for scaling_decision_option in 1; do # 2 0
       for autotuner_increase_bar_alpha in 0.1 0.2 0.4; do #
-        for L in 3500; do # 1000 1500 2000 2500 3000
+        for L in 3500 4000; do # 1000 1500 2000 2500 3000
             whether_type="streamsluice"
             how_type="streamsluice"
             scalein_type="streamsluice"
