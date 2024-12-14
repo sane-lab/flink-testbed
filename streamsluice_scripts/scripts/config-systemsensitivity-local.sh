d6551c0d72115a30b2d545364532a99eb0ecf627
swrrt <swrrt.11@gmail.com>
2024-12-14 16:51:32 +0800

    Debugging

--- streamsluice_scripts/scripts/config-systemsensitivity-local.sh
+++ streamsluice_scripts/scripts/config-systemsensitivity-local.sh
@@ -82,7 +82,6 @@ function configFlink() {
     cp ${FLINK_CONF_DIR}/* ${FLINK_DIR}/conf
     cp ${FLINK_CONF_DIR}/slaves_systemsensitivity ${FLINK_DIR}/conf/slaves
     cp ${FLINK_CONF_DIR}/flink-conf-systemsensitivity.yaml ${FLINK_DIR}/conf/flink-conf.yaml
-#    cp ${FLINK_CONF_DIR}/slaves_systemsensitivity ${FLINK_DIR}/conf/slaves
 #    for host in "dragon" "eagle"; do
 #      scp ${FLINK_CONF_DIR}/* ${host}:${FLINK_DIR}/conf
 #      scp ${FLINK_CONF_DIR}/flink-conf-slave-systemsensitivity.yaml ${host}:${FLINK_DIR}/conf/flink-conf.yaml
