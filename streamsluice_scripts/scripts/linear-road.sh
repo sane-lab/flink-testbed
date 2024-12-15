bade1dc03436ac193f3441584d7bc297eab80c81
swrrt <swrrt.11@gmail.com>
2024-12-16 01:18:06 +0800

    Debugging

--- streamsluice_scripts/scripts/linear-road.sh
+++ streamsluice_scripts/scripts/linear-road.sh
@@ -15,7 +15,7 @@ function analyze() {
     mv ${EXP_DIR}/streamsluice/ ${EXP_DIR}/raw/${EXP_NAME}
     mkdir ${EXP_DIR}/streamsluice/
 
-    for host in "dragon" "eagle"; do
+    for host in "dragon" ; do # "eagle"
       scp ${host}:${FLINK_DIR}/log/* ${EXP_DIR}/raw/${EXP_NAME}/
       ssh ${host} "rm ${FLINK_DIR}/log/*"
     done
@@ -184,12 +184,12 @@ run_stock_test(){
     repeat=2
     for scaling_decision_option in 1; do # 2 0
       for autotuner_increase_bar_alpha in 0.1; do # 0.1 0.2 0.4
-        for L in 2000; do # 1000 1500 2000 2500 3000
+        for L in 1000 1500 2000 2500 3000; do #
             whether_type="streamsluice"
             how_type="streamsluice"
             scalein_type="streamsluice"
-            run_one_exp
-            printf "${EXP_NAME}\n" >> lr_result.txt
+#            run_one_exp
+#            printf "${EXP_NAME}\n" >> lr_result.txt
         done
       done
     done
@@ -247,32 +247,32 @@ run_stock_test(){
     how_type="streamsluice"
     scalein_type="streamsluice"
     printf "Part_3\n" >> lr_result.txt
-#    printf "Epoch Length\n" >> lr_result.txt
-#    for epoch in 25 50 200 500; do
-#      for L in 2000; do
-#        run_one_exp
-#        printf "${EXP_NAME}\n" >> lr_result.txt
-#      done
-#    done
-#    epoch=100
+    printf "Epoch Length\n" >> lr_result.txt
+    for epoch in 25 50 100 200 500; do
+      for L in 2000; do
+        run_one_exp
+        printf "${EXP_NAME}\n" >> lr_result.txt
+      done
+    done
+    epoch=100
 
     printf "Tuning window Length\n" >> lr_result.txt
-#    for autotune_interval in 15 30 90 120; do
-#      for L in 2000; do
-#        run_one_exp
-#        printf "${EXP_NAME}\n" >> lr_result.txt
-#      done
-#    done
-#    autotune_interval=60
+    for autotune_interval in 15 30 90 120; do
+      for L in 2000; do
+        run_one_exp
+        printf "${EXP_NAME}\n" >> lr_result.txt
+      done
+    done
+    autotune_interval=60
 
     printf "Alpha\n" >> lr_result.txt
-#    for autotuner_increase_bar_alpha in 0.2 0.3 0.4 0.5; do
-#      for L in 2000; do
-#        run_one_exp
-#        printf "${EXP_NAME}\n" >> lr_result.txt
-#      done
-#    done
-#    autotuner_increase_bar_alpha=0.1
+    for autotuner_increase_bar_alpha in 0.2 0.3 0.4 0.5; do
+      for L in 2000; do
+        run_one_exp
+        printf "${EXP_NAME}\n" >> lr_result.txt
+      done
+    done
+    autotuner_increase_bar_alpha=0.1
 
 }
 run_stock_test
