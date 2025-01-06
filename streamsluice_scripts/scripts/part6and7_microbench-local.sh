f22c4042604ab9d5866795eaa42e5d413b817ead
swrrt <swrrt.11@gmail.com>
2025-01-06 12:23:26 +0800

    Debugging

--- streamsluice_scripts/scripts/part6and7_microbench-local.sh
+++ streamsluice_scripts/scripts/part6and7_microbench-local.sh
@@ -236,12 +236,12 @@ run_scale_test(){
 #    printf "${EXP_NAME}\n" >> part6and7_result.txt
     how_type="streamsluice"
     for L in 1000; do # 2000 3000
-      for whether_type in "dhalion"; do # "streamsluice" "ds2" "dhalion" "streamswitch"
-        for repeat in 1 2 3; do
+      for whether_type in "streamsluice" "ds2" "dhalion" "streamswitch"; do #
+        for repeat in 1; do
           is_treat=true
           autotune=false
-          run_one_exp
-          printf "${EXP_NAME}\n" >> part6and7_result.txt
+#          run_one_exp
+#          printf "${EXP_NAME}\n" >> part6and7_result.txt
         done
       done
     done
@@ -249,12 +249,12 @@ run_scale_test(){
     SOURCE_TYPE="part7"
     whether_type="streamsluice"
     for L in 1000; do # 2000 3000
-      for how_type in "ds2" "drs" "streamswitch"; do
-        for repeat in 1; do
+      for how_type in "drs"; do # "ds2" "drs" "streamswitch"
+        for repeat in 1 2 3; do
           is_treat=true
           autotune=false
-#          run_one_exp
-#          printf "${EXP_NAME}\n" >> part6and7_result.txt
+          run_one_exp
+          printf "${EXP_NAME}\n" >> part6and7_result.txt
         done
       done
     done
