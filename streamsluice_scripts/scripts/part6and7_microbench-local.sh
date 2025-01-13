490b5f6defa42c539d5e5a96550645c25c0d628d
swrrt <swrrt.11@gmail.com>
2025-01-13 12:20:19 +0800

    Debugging

--- streamsluice_scripts/scripts/part6and7_microbench-local.sh
+++ streamsluice_scripts/scripts/part6and7_microbench-local.sh
@@ -235,19 +235,21 @@ run_scale_test(){
 
     runtime=120 # 120
     for CURVE_TYPE in "linear"; do # "sine" "gradient"
-      for amplitude_low in 2000; do # 2000 1000
-        is_treat=false
-        autotune=false
-        how_type="ds2"
-#        run_one_exp
-#        printf "${EXP_NAME}\n" >> part6and7_result.txt
-        how_type="streamsluice"
-        for whether_type in "streamsluice" "streamsluice_earlier" "streamsluice_later"; do #  "ds2" "dhalion" "streamswitch";
-          for repeat in 1 2 3; do #
-            is_treat=true
-            autotune=true
-            run_one_exp
-            printf "${EXP_NAME}\n" >> part6and7_result.txt
+      for autotuner_initial_value_alpha in 600 700 800; do
+        for amplitude_low in 2000; do # 2000 1000
+          is_treat=false
+          autotune=false
+          how_type="ds2"
+  #        run_one_exp
+  #        printf "${EXP_NAME}\n" >> part6and7_result.txt
+          how_type="streamsluice"
+          for whether_type in "streamsluice" "streamsluice_earlier" "streamsluice_later"; do #  "ds2" "dhalion" "streamswitch";
+            for repeat in 1 2 3; do #
+              is_treat=true
+              autotune=true
+              run_one_exp
+              printf "${EXP_NAME}\n" >> part6and7_result.txt
+            done
           done
         done
       done
