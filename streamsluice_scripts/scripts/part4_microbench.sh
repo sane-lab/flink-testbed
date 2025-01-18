2b3cdb332c0242c0f566cf11a58504be806b890d
swrrt <swrrt.11@gmail.com>
2025-01-18 23:23:01 +0800

    Debugging

--- streamsluice_scripts/scripts/part4_microbench.sh
+++ streamsluice_scripts/scripts/part4_microbench.sh
@@ -256,7 +256,7 @@ run_scale_test(){
     autotuner_increase_bar_option=10
 
     for autotuner_initial_value_alpha in 500 1000 1500 2000 2500 3000; do #250 500 1000 1500 2000 2500 3000
-      for repeat in 1 2 3;
+      for repeat in 1 2 3; do
         is_treat=true
         autotune=true
         how_type="streamsluice"
