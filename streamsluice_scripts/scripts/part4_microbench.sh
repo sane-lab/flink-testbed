5d20e1ff9ffe420f0a6850d7800da0ebbd15da3c
swrrt <swrrt.11@gmail.com>
2025-01-18 22:12:10 +0800

    Debugging

--- streamsluice_scripts/scripts/part4_microbench.sh
+++ streamsluice_scripts/scripts/part4_microbench.sh
@@ -192,7 +192,7 @@ run_scale_test(){
     printf "MicroBench Intrinsic Bound Tuning Verification\n" >> part4_result.txt
     autotuner_initial_value_alpha=1.2
     L=2000
-    runtime=1880 #1880
+    runtime=380 #1880
     setting="microbench"
     SOURCE_TYPE="part4"
     DELAY2=20
@@ -224,14 +224,14 @@ run_scale_test(){
     TIME_I=0
     PHASE_TIMES="600-600-600"
     PHASE_RATES="5000-5000-5000"
-    PHASE_AMPLITUDES="7250-8500-4000" #"8000-2500-7000"
+    PHASE_AMPLITUDES="3000-8000-3000"#"7250-8000-3000" #"8000-2500-7000"
     PHASE_PERIODS="30-30-60" # Total period for each (60 + x)
     PHASE_TYPES="sine-sine-sine"
 
     is_treat=false
     autotune=false
     how_type="ds2"
-    autotune_interval=120 #120
+    autotune_interval=90 #120
 
   #    run_one_exp
   #    printf "${EXP_NAME}\n" >> part4_result.txt
