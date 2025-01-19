c5e5d07549a73cb07ce62c2634dede650553c62e
swrrt <swrrt.11@gmail.com>
2025-01-19 17:57:56 +0800

    Debugging

--- streamsluice_scripts/scripts/part4_microbench.sh
+++ streamsluice_scripts/scripts/part4_microbench.sh
@@ -234,8 +234,8 @@ run_scale_test(){
     autotune_interval=120 #90
 
     P3=22
-    run_one_exp
-    printf "${EXP_NAME}\n" >> part4_result.txt
+#    run_one_exp
+#    printf "${EXP_NAME}\n" >> part4_result.txt
 
     P3=20
 
@@ -258,13 +258,13 @@ run_scale_test(){
     autotuner_adjustment_option=10 # no adjustment
     autotuner_increase_bar_option=10
 
-    for autotuner_initial_value_alpha in 500 1000 1500 2000 2500 3000; do #250 500 1000 1500 2000 2500 3000
+    for autotuner_initial_value_alpha in 250; do #250 500 1000 1500 2000 2500 3000
       for repeat in 1 2 3; do
         is_treat=true
         autotune=true
         how_type="streamsluice"
-#        run_one_exp
-#        printf "${EXP_NAME}\n" >> part4_result.txt
+        run_one_exp
+        printf "${EXP_NAME}\n" >> part4_result.txt
       done
     done
 }
