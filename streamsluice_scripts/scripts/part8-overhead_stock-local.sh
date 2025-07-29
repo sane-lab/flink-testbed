b4280cda154614413beafacd0bf3e0a4224e4dd4
swrrt <swrrt.11@gmail.com>
2025-07-29 21:20:56 +0800

    Debugging

--- streamsluice_scripts/scripts/part8-overhead_stock-local.sh
+++ streamsluice_scripts/scripts/part8-overhead_stock-local.sh
@@ -380,7 +380,7 @@ run_stock_test(){
     how_type="streamsluice"
     scalein_type="streamsluice"
     for metrics_report_interval in 100000000; do  # 5000000 25000000
-      for p7 in 1 5 10; do
+      for P7 in 1 5 10 20; do
         for repeat in 1; do
           run_one_exp
           printf "${EXP_NAME}\n" >> part8_result.txt
@@ -397,7 +397,7 @@ run_stock_test(){
     whether_type="streamsluice"
     how_type="streamsluice"
     scalein_type="streamsluice"
-    for p7 in 1 5 10; do
+    for P7 in 1 5 10 20; do
         for repeat in 1; do
         run_one_exp
         printf "${EXP_NAME}\n" >> part8_result.txt
